de655ed06b254098b1604e9f26f2315399dd8113
Gaurav Madan <gauravmadan2004@gmail.com>
2025-08-07 01:38:59 +0530

    convert any image format to .jpg with compressed size

--- app/services/aws_bucket.py
+++ app/services/aws_bucket.py
@@ -1,33 +1,50 @@
 import os
 import datetime
 import boto3
+import io
+from PIL import Image, UnidentifiedImageError
+from pillow_heif import register_heif_opener
 from botocore.exceptions import NoCredentialsError
 from app.config import Config
+
+# Register HEIF support
+register_heif_opener()
 
 def handle_image_upload(file):
     try:
         s3 = boto3.client(
             's3',
-            aws_access_key_id = Config.AWS_ACCESS_KEY,
-            aws_secret_access_key = Config.AWS_SECRET_ACCESS_KEY,
-            region_name = Config.AWS_REGION
+            aws_access_key_id=Config.AWS_ACCESS_KEY,
+            aws_secret_access_key=Config.AWS_SECRET_ACCESS_KEY,
+            region_name=Config.AWS_REGION
         )
 
-        name,ext = os.path.splitext(file.filename)
+        name, ext = os.path.splitext(file.filename)
         now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-        unique_filename = f"{name}_{now}{ext}"
-
+        unique_filename = f"{name}_{now}.jpg"
         key = f"wave_length_assets/{unique_filename}"
 
+        try:
+            image = Image.open(file.stream)
+        except UnidentifiedImageError:
+            raise Exception("Invalid image file")
+
+        image = image.convert("RGB")  # Convert to JPEG compatible mode
+        image_io = io.BytesIO()
+        image.save(image_io, format="JPEG", quality=40)
+        image_io.seek(0)
+
         s3.upload_fileobj(
-            file,
+            image_io,
             Config.AWS_S3_BUCKET_NAME,
             key,
-            ExtraArgs={"ContentType": file.content_type}
+            ExtraArgs={"ContentType": "image/jpeg"}
         )
 
-        image_url=f"https://{Config.AWS_S3_BUCKET_NAME}.s3.{Config.AWS_REGION}.amazonaws.com/{key}"
+        image_url = f"https://{Config.AWS_S3_BUCKET_NAME}.s3.{Config.AWS_REGION}.amazonaws.com/{key}"
         return image_url
 
     except NoCredentialsError:
-        raise Exception("AWS credentials not available")    
+        raise Exception("AWS credentials not available")
+    except Exception as e:
+        raise Exception(f"Image upload failed: {str(e)}")
